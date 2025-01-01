7e6b13cf8b2ebb4f7d96c4b3025e07bada3bcc6e
rounak <rounak.aftab@gmail.com>
2025-01-01 12:44:17 -0500

    Check for formatting &  linting

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 # flake8, blackpi
 
-# def sum():
 
-#     a = 5
-#     b = 10
-#     return a + b
-
-
-# sum()
+def sum():
+    a = 5
+    b = 10
+    return a + b
 
 
-def add(a, b):return a + b
+sum()
 
 
-result = add(5 + 10); print(result)
+def add(a, b):
+    return a + b
+
+
+result = add(5 + 10)
+print(result)
